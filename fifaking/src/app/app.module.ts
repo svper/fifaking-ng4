32f047405d4399c44cff5a1f43b4c2af7d1c9c1b
Robin Verhulst <verhulstrobin@gmail.com>
2017-12-08 14:22:59 +0100

    added match list component

--- fifaking/src/app/app.module.ts
+++ fifaking/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { MatchListComponent } from './match-list/match-list.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MatchListComponent
   ],
   imports: [
     BrowserModule
---
+++ fifaking/src/app/match-list/match-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'fk-match-list',
+  templateUrl: './match-list.component.html',
+  styleUrls: ['./match-list.component.scss']
+})
+export class MatchListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
